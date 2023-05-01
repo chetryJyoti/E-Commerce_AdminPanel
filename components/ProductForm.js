d11b2bd722f491bf38b07b9d0aba1f849fd48370
chetryJyoti <jyotichetry087@gmail.com>
2023-05-02 02:11:16 +0530

    Added file upload functionality to S3 bucket using multiparty and AWS SDK also also added  sortability to images

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -1,25 +1,28 @@
 import axios from "axios";
 import { useRouter } from "next/router";
 import { useState } from "react";
-
+import ClipLoader from "react-spinners/ClipLoader";
+import { ReactSortable } from "react-sortablejs";
 const ProductForm = ({
   _id,
   title: currentTitle,
   description: currentDesc,
   price: currentPrice,
-  images,
+  images: currentImages,
 }) => {
   const [title, setTitle] = useState(currentTitle || "");
   const [description, setDescription] = useState(currentDesc || "");
   const [price, setPrice] = useState(currentPrice || "");
+  const [images, setImages] = useState(currentImages || []);
   const [goToProducts, setGoToProducts] = useState(false);
+  const [isUploading, setIsUploading] = useState(false);
   const router = useRouter();
 
   // console.log("product_id:", _id);
 
   const saveProduct = async (ev) => {
     ev.preventDefault();
-    const data = { title, description, price };
+    const data = { title, description, price, images };
     //update
     if (_id) {
       await axios.put("/api/products", { ...data, _id });
@@ -34,20 +37,24 @@ const ProductForm = ({
     const files = ev.target?.files;
     console.log(files);
     if (files?.length > 0) {
+      setIsUploading(true);
       const data = new FormData();
       for (const file of files) {
         data.append("file", file);
       }
-      // const res = await axios.post("/api/uploadImg", data, {
-      //   headers: { "Content-Type": "multipart/form-data" },
-      // });
-      const res = await fetch('/api/uploadImg',{
-        method:"POST",
-        body:data,
-      })
-      console.log(res);
+      const res = await axios.post("/api/uploadImg", data);
+      console.log(res.data);
+      setImages((oldImages) => {
+        return [...oldImages, ...res.data.links];
+      });
     }
+    setIsUploading(false);
   }
+
+ function updateImagesOrder(images){
+  // console.log(images);
+  setImages(images);
+ }
 
   if (goToProducts) {
     router.push("/products");
@@ -70,7 +77,21 @@ const ProductForm = ({
         onChange={(e) => setDescription(e.target.value)}
       ></textarea>
       <label>Upload Photos:</label>
-      <div className="my-2">
+      <div className="my-2 flex flex-wrap gap-2 ">
+        <ReactSortable className="flex flex-wrap gap-2 cursor-pointer " list={images} setList={updateImagesOrder}>
+          {!!images?.length &&
+            images.map((link) => (
+              <div key={link} className="h-24 ">
+                <img src={link} className="rounded-md" />
+              </div>
+            ))}
+        </ReactSortable>
+        {isUploading && (
+          <div className="w-24 h-24 cursor-pointer bg-gray-200 text-gray-500 flex flex-col justify-center items-center rounded-md">
+            <ClipLoader size={25} color={"#123abc"} />
+            <p>Uploading </p>
+          </div>
+        )}
         <label className="w-24 h-24 cursor-pointer bg-gray-200 text-gray-500 flex flex-col justify-center items-center rounded-md">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -89,6 +110,9 @@ const ProductForm = ({
           <div>Upload</div>
           <input type="file" className="hidden" onChange={uploadImages} />
         </label>
+      </div>
+      <div className="my-3">
+        {" "}
         {!images?.length && <div>No images for this product</div>}
       </div>
       <label>Price ( ₹ )</label>
--- pages/api/uploadImg.js
+++ pages/api/uploadImg.js
@@ -1,15 +1,51 @@
 import multiparty from "multiparty";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import fs from "fs";
+import mime from "mime-types";
+const bucketName = "admin-ecommerce-nextjs";
 
 export default async function handle(req, res) {
   const form = new multiparty.Form();
 
-  form.parse(req, (err, fields, files) => {
-    if (err) throw err;
-    console.log("length:",files.file.length);
-    console.log("fields:",fields);
-    res.json("ok");
+  const { fields, files } = await new Promise((resolve, reject) => {
+    form.parse(req, (err, fields, files) => {
+      if (err) reject(err);
+      else resolve({ fields, files });
+    });
   });
+  console.log("file", files);
+  console.log("length:", files.file.length);
+  // console.log("fields:", fields);
+
+  const client = new S3Client({
+    region: "ap-south-1",
+    credentials: {
+      accessKeyId: process.env.S3_ACCESS_KEY,
+      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+    },
+  });
+
+  const links = [];
+  for (const file of files.file) {
+    const ext = file.originalFilename.split(".").pop();
+    const newFileName = Date.now() + "." + ext;
+    console.log("extension:", ext);
+    await client.send(
+      new PutObjectCommand({
+        Bucket: bucketName,
+        Key: newFileName,
+        Body:fs.readFileSync(file.path),
+        ACL: "public-read",
+        ContentType: mime.lookup(file.path),
+      })
+    );
+    const link = `https://${bucketName}.s3.amazonaws.com/${newFileName}`;
+    links.push(link);
+  }
+
+  return res.json({ links });
 }
+
 export const config = {
   api: { bodyParser: false },
 };
