ee96d9fb993384542432307b6a385a33ed2693e2
chetryJyoti <jyotichetry087@gmail.com>
2023-05-15 16:19:46 +0530

    ui fix

--- components/Loader.js
+++ components/Loader.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const Loader = ({loadingWhat}) => {
   return (
-    <div className=" mt-8 flex flex-col justify-center items-center ">
+    <div className="mt-8 flex flex-col justify-center items-center ">
       <h1>{loadingWhat}</h1>
       <ClipLoader size={50} color={"#123abc"} />
     </div>
